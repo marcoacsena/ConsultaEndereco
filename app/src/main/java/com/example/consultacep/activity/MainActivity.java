285f92727610a716f468245e5680f01adad8568c
marco sena <marcoacsena@gmail.com>
2020-08-13 16:50:43 -0300

    Aplicação Conculta CEP com uso de HashMap.

--- app/src/main/java/com/example/consultacep/activity/MainActivity.java
+++ app/src/main/java/com/example/consultacep/activity/MainActivity.java
@@ -15,6 +15,9 @@ import com.example.consultacep.api.RetrofitClientInstance;
 import com.example.consultacep.model.Cep;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.HashMap;
+
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -57,34 +60,57 @@ public class MainActivity extends AppCompatActivity {
     private void obterCep(String cepDigitado) {
 
         CepApi cepApi = RetrofitClientInstance.getRetrofitInstance().create(CepApi.class);
-        Call<Cep> call = cepApi.getCep(cepDigitado);
+        Call<HashMap<String, String>> call = cepApi.getCep(cepDigitado);
 
-        call.enqueue(new Callback<Cep>() {
+        call.enqueue(new Callback<HashMap<String, String>>() {
             @Override
-            public void onResponse(Call<Cep> call, Response<Cep> response) {
+            public void onResponse(Call<HashMap<String, String>> call, Response<HashMap<String, String>> response) {
+
                 if(response.isSuccessful()){
 
+                    //Cep cep = response.body();
+//                    String logradouro;
+//                    logradouro = "- Logradouro: " +cep.getLogradouro() +"\n";
 
-                    Cep cep = response.body();
 
-                    String logradouro;
-                    logradouro = "- Logradouro: " +cep.getLogradouro() +"\n";
+                    HashMap<String, String> cep = response.body();
 
-                    tvResultado.setText(logradouro +"- Complemento: " +cep.getComplemento() +"\n"
-                            + "- Bairro: " +cep.getBairro() +"\n" + "- Cidade: " +cep.getCidade() +"\n" +"- Cep.: " +cep.getCep());
+//                    tvResultado.setText("logradouro: " +cep.getLogradouro() +"\n" +"- Complemento: "
+//                            +cep.getComplemento() +"\n" + "- Bairro: " +cep.getBairro() +"\n"
+//                            + "- Cidade: " +cep.getCidade() +"\n" +"- Cep.: " +cep.getCep());
+
+                    String endereco = "";
+
+                    for(String item: cep.keySet()){
+                        endereco+= "- " +item +": " +cep.get(item)  +"\n";
+
+                    }
+
+                    tvResultado.setText(endereco);
 
                     tvResultado.setVisibility(View.VISIBLE);
                     progressBar.setVisibility(View.INVISIBLE);
+                }else{
+
+                    int codigoErro = response.code();
+                    Toast.makeText(MainActivity.this, "Código do erro: " +codigoErro,
+                            Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
-            public void onFailure(Call<Cep> call, Throwable t) {
+            public void onFailure(Call<HashMap<String, String>> call, Throwable t) {
+
                 Toast.makeText(MainActivity.this, "Não foi possível obter o cep!", Toast.LENGTH_SHORT).show();
 
             }
         });
 
 
+    }
+
+    public void fazerNovaConsulta(View view) {
+
+        cep.setText("");
     }
 }
--- app/src/main/java/com/example/consultacep/api/CepApi.java
+++ app/src/main/java/com/example/consultacep/api/CepApi.java
@@ -2,12 +2,16 @@ package com.example.consultacep.api;
 
 import com.example.consultacep.model.Cep;
 
+import java.util.HashMap;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface CepApi {
 
+
+
     @GET("{CEP}/json")
-    Call<Cep> getCep(@Path("CEP") String CEP);
+    Call<HashMap<String,String>> getCep(@Path("CEP") String CEP);
 }
